5864a0900d04c56e2f5bd90ae3b1110fbb2c3977
Christian T Flink <chri62aa@edu.easj.dk>
2018-10-21 14:37:05 +0200

    kap 4 frem til 4_22

--- src/Opg4_22.java
+++ src/Opg4_22.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Opg4_22 {
     public static void main(String[] args) {
 
+
         String a ="";
         String b ="";
         Scanner input=new Scanner(System.in);
